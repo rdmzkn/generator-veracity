40f1a9c105b0eb8bac0cd09aa4346879e480c63c
Thomas <thomas.rudfoss@gmail.com>
2019-05-10 08:07:21 +0200

    Added refresh button to user

--- generators/node-spa/template/client/src/features/User/User.connector.js
+++ generators/node-spa/template/client/src/features/User/User.connector.js
@@ -5,7 +5,10 @@ import * as user from "../../ducks/user.duck"
 
 export default connect((state) => ({
 	isAuthenticated: user.isAuthenticated(state),
+	isLoading: user.isLoading(state),
 	id: user.getId(state),
 	fullName: user.getFullName(state),
 	email: user.getEmail(state)
+}), (dispatch) => ({
+	onReload: () => dispatch(user.fetchUser())
 }))(User)
--- generators/node-spa/template/client/src/features/User/User.js
+++ generators/node-spa/template/client/src/features/User/User.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-export const User = ({ isAuthenticated, id, fullName, email }) => {
+export const User = ({ isAuthenticated, isLoading, id, fullName, email, onReload }) => {
 	if (!isAuthenticated) return (
 		<div>Please <a href="/login">log in</a> and revisit this page.</div>
 	)
@@ -21,14 +21,25 @@ export const User = ({ isAuthenticated, id, fullName, email }) => {
 					<td>Email</td>
 					<td>{email}</td>
 				</tr>
+				<tr>
+					<td>Refresh</td>
+					<td><button onClick={onReload}>Refresh user data</button>{isLoading && (<span>Loading... </span>)}</td>
+				</tr>
+				<tr>
+					<td>Log out</td>
+					<td><a href="/logout">Log out</a></td>
+				</tr>
 			</tbody>
 		</table>
 	)
 }
 User.propTypes = {
 	isAuthenticated: PropTypes.bool.isRequired,
+	isLoading: PropTypes.bool,
 	id: PropTypes.string,
 	fullName: PropTypes.string,
-	email: PropTypes.string
+	email: PropTypes.string,
+
+	onReload: PropTypes.func
 }
 export default User
